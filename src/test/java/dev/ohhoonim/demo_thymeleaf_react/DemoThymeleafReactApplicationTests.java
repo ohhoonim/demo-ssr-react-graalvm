c2a24465559f65a71eccc23ad6f8106b15195fdd
mathew <donnj95@gmail.com>
2025-09-22 13:16:16 +0900

    first commit

---
+++ src/test/java/dev/ohhoonim/demo_thymeleaf_react/DemoThymeleafReactApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ohhoonim.demo_thymeleaf_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoThymeleafReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
